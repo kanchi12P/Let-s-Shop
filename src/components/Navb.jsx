a289248c41e34d6ef5f97010a5b93f3a655d1f96
kanchi12P <kanchipardhi@gmail.com>
2021-12-07 11:31:36 +0530

    added

--- src/components/Navb.jsx
+++ src/components/Navb.jsx
@@ -6,7 +6,7 @@ function Navb(props) {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Let's Shop</Navbar.Brand>
+        <Navbar.Brand ><NavLink to="/">Let's Shop</NavLink></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
